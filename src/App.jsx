c52b48c94fd57d18cc4b8fb8e2dcc0cf517e7e58
choihyunjii <such1123@naver.com>
2025-02-03 17:23:59 +0900

    43
    유튭 강의로 익힌 기본 개념 다시 한 번 공부함 옛날에 들었던 부분인데 당시에는 이해못하고 들었는데 확실히 개념 익히고 들으니까 전보다 이해력 상승

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,60 @@
-function App() {
-  return (
-    <div>
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header(){
+
+  const description = reactDescriptions[genRandomInt(2)]
+
+  return(
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+  );
+}
+
+
+function CoreComponent(props){
+  return(
+    <li>
+      <img src={props.image} alt={props.title}/>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
       <main>
+        <section id="core-concepts">
+          <ul>
+            <CoreComponent
+              title="Components"
+              description="The core UI building block"
+              image={componentsImg}
+            />
+            <CoreComponent/>
+            <CoreComponent/>
+            
+
+          </ul>
+         
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
